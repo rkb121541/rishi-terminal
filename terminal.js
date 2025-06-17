2073db9e682262ebdd43dd95e34a5572255ec1e7
rkb121541 <rkbidarkota@gmail.com>
2025-06-16 23:38:03 -0400

    added colors

--- terminal.js
+++ terminal.js
@@ -1,69 +1,57 @@
+const colors = {
+  orange: getComputedStyle(document.documentElement)
+    .getPropertyValue("--orange")
+    .trim(),
+  blue: getComputedStyle(document.documentElement)
+    .getPropertyValue("--blue")
+    .trim(),
+  purple: getComputedStyle(document.documentElement)
+    .getPropertyValue("--purple")
+    .trim(),
+  green: getComputedStyle(document.documentElement)
+    .getPropertyValue("--green")
+    .trim(),
+  red: getComputedStyle(document.documentElement)
+    .getPropertyValue("--red")
+    .trim(),
+};
+
 function showWelcome() {
   return `
+[[;${colors.green};]
 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
 ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
 ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
 ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
 ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
- ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
-
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝]
 ${showHelp()}
-`;
-}
-
-function showHello() {
-  return `
-██╗  ██╗███████╗██╗     ██╗      ██████╗ 
-██║  ██║██╔════╝██║     ██║     ██╔═══██╗
-███████║█████╗  ██║     ██║     ██║   ██║
-██╔══██║██╔══╝  ██║     ██║     ██║   ██║
-██║  ██║███████╗███████╗███████╗╚██████╔╝
-╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ 
-`;
-}
-
-function showHi() {
-  return `
-██╗  ██╗██╗
-██║  ██║██║
-███████║██║
-██╔══██║██║
-██║  ██║██║
-╚═╝  ╚═╝╚═╝
 `;
 }
 
 function showHelp() {
   return `
+[[;${colors.orange};]
 Available commands:
   help        - Displays this help menu
-  hello       - Displays the 'HELLO' banner
-  hi          - Displays the 'HI' banner
   whoareyou   - About me
   whoami      - About you
   email       - My Email
   linkedin    - My Linkedin
   github      - My Github
-  website     - My other website (coming soon)
 
-Feel free to try out some other commands :)
+Feel free to try out some other commands :)]
 `;
 }
 
 $("body").terminal(
-  function (command, term) {
+  function(command, term) {
     const cmd = command.trim().toLowerCase();
     switch (true) {
       case cmd === "":
         break;
       case cmd === "help":
         term.echo(showHelp());
-        break;
-      case cmd === "hello":
-        term.echo(showHello());
-        break;
-      case cmd === "hi":
-        term.echo(showHi());
         break;
       case cmd === "whoareyou":
         term.echo(
@@ -74,7 +62,7 @@ $("body").terminal(
         term.echo("\nI don't know who you are. Why are you asking me?\n");
         break;
       case cmd === "email":
-        term.echo("\nEmail me at rbidarko@stevens.edu\n");
+        term.echo(`\nEmail me at [[;${colors.blue};]rbidarko@stevens.edu]\n`);
         break;
       case cmd === "linkedin":
         term.echo("\nOpening my Linkedin...\n");
@@ -84,31 +72,36 @@ $("body").terminal(
         term.echo("\nOpening my Github...\n");
         window.open("https://github.com/rkb121541", "_blank");
         break;
-      case cmd === "website":
-        term.echo("\nNot ready yet, coming soon.\n");
-        // window.open("", "_blank");
-        break;
       case cmd.startsWith("sudo"):
-        term.echo("\nWhat makes you think you have administrative privileges?\n");
-        window.open("https://www.youtube.com/watch?v=nPrWo5pEvyk", "_blank");
+        term.echo(
+          `[[;${colors.purple};]\nWhat makes you think you have administrative privileges?\n]`,
+        );
+        setTimeout(() => {
+          window.open("https://www.youtube.com/watch?v=nPrWo5pEvyk", "_blank");
+        }, 1000);
         break;
       case cmd.startsWith("rm"):
-        term.echo("\nWhy are you trying to remove things?\n");
-        window.open("https://www.youtube.com/watch?v=LpsIp5KTeug", "_blank");
+        term.echo(
+          `[[;${colors.purple};]\nWhy are you trying to remove things ?\n]`,
+        );
+        setTimeout(() => {
+          window.open("https://www.youtube.com/watch?v=LpsIp5KTeug", "_blank");
+        }, 1000);
         break;
       default:
-        term.echo(`\nCommand not found: ${command}\n`);
+        term.echo(`[[;${colors.red};]\nCommand not found: ${command} \n]`);
         break;
     }
   },
   {
     greetings: showWelcome(),
+    render: true,
     keymap: {
-      "CTRL+R": function () {
+      "CTRL+R": function() {
         return false;
       },
     },
-    keydown: function (e) {
+    keydown: function(e) {
       if (e.which == 82 && e.ctrlKey) {
         return true;
       }
